e7c750ce55737d56ae580e8725a21a3d285f5653
ganma <ganmacs@gmail.com>
2014-04-02 22:35:28 +0900

    フォローボタン(ajax対応)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,21 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to @user
+
+    respond_to do |format|
+      format.js
+      format.html { redirect_to @user }
+    end
   end
 
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to @user
+
+    respond_to do |format|
+      format.js
+      format.html { redirect_to @user }
+    end
   end
 
 end
